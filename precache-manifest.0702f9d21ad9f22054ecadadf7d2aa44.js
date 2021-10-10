85bf28464dccd6070d1ba685cc5aab017db16b7f
yaoba <7156147@qq.com>
2021-10-10 14:28:59 +0800

    网站发布

---
+++ precache-manifest.0702f9d21ad9f22054ecadadf7d2aa44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "933fe3ad4e88705ce0b6",
+    "url": "./static/css/main.1bb19b2a.chunk.css"
+  },
+  {
+    "revision": "933fe3ad4e88705ce0b6",
+    "url": "./static/js/main.933fe3ad.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.8c97409f.js"
+  },
+  {
+    "revision": "77fc0a13fb1fe624ef19",
+    "url": "./static/css/2.1076cb33.chunk.css"
+  },
+  {
+    "revision": "77fc0a13fb1fe624ef19",
+    "url": "./static/js/2.77fc0a13.chunk.js"
+  },
+  {
+    "revision": "6f53e515fb83b631764a6aabdb01ee2f",
+    "url": "./index.html"
+  }
+];
